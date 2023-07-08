de1e717c3a001c2fb90797537ab8df8894cef986
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-07-07 23:51:18 -0700

    update snapshots

--- test/FullMathGas.t.sol
+++ test/FullMathGas.t.sol
@@ -5,77 +5,96 @@ import {FullMath} from "../src/FullMath.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 import {Test} from "forge-std/Test.sol";
 
+contract FullMathWrapper {
+    FullMath public fm;
+
+    constructor() {
+        // fm = new FullMath();
+        fm = FullMath(HuffDeployer.deploy("FullMathHuff"));
+    }
+}
+
 /**
  * @dev Tests copied from https://github.com/Vectorized/solady/blob/main/test/FixedPointMathLib.t.sol
  */
-contract FullMathHuffTest is Test {
-    FullMath fullMathHuff;
+contract FullMathTest is Test {
+    FullMath fullMathLibrary;
 
     function setUp() public {
-        fullMathHuff = FullMath(HuffDeployer.deploy("FullMathHuff"));
+        FullMathWrapper wrapper = new FullMathWrapper();
+        fullMathLibrary = wrapper.fm();
     }
 
     function testMulDiv() public {
-        assertEq(fullMathHuff.mulDiv(2.5e27, 0.5e27, 1e27), 1.25e27);
-        assertEq(fullMathHuff.mulDiv(2.5e18, 0.5e18, 1e18), 1.25e18);
-        assertEq(fullMathHuff.mulDiv(2.5e8, 0.5e8, 1e8), 1.25e8);
-        assertEq(fullMathHuff.mulDiv(369, 271, 1e2), 999);
+        assertEq(fullMathLibrary.mulDiv(2.5e27, 0.5e27, 1e27), 1.25e27);
+        assertEq(fullMathLibrary.mulDiv(2.5e18, 0.5e18, 1e18), 1.25e18);
+        assertEq(fullMathLibrary.mulDiv(2.5e8, 0.5e8, 1e8), 1.25e8);
+        assertEq(fullMathLibrary.mulDiv(369, 271, 1e2), 999);
 
-        assertEq(fullMathHuff.mulDiv(1e27, 1e27, 2e27), 0.5e27);
-        assertEq(fullMathHuff.mulDiv(1e18, 1e18, 2e18), 0.5e18);
-        assertEq(fullMathHuff.mulDiv(1e8, 1e8, 2e8), 0.5e8);
+        assertEq(fullMathLibrary.mulDiv(1e27, 1e27, 2e27), 0.5e27);
+        assertEq(fullMathLibrary.mulDiv(1e18, 1e18, 2e18), 0.5e18);
+        assertEq(fullMathLibrary.mulDiv(1e8, 1e8, 2e8), 0.5e8);
 
-        assertEq(fullMathHuff.mulDiv(2e27, 3e27, 2e27), 3e27);
-        assertEq(fullMathHuff.mulDiv(3e18, 2e18, 3e18), 2e18);
-        assertEq(fullMathHuff.mulDiv(2e8, 3e8, 2e8), 3e8);
+        assertEq(fullMathLibrary.mulDiv(2e27, 3e27, 2e27), 3e27);
+        assertEq(fullMathLibrary.mulDiv(3e18, 2e18, 3e18), 2e18);
+        assertEq(fullMathLibrary.mulDiv(2e8, 3e8, 2e8), 3e8);
     }
 
     function testMulDivEdgeCases() public {
-        assertEq(fullMathHuff.mulDiv(0, 1e18, 1e18), 0);
-        assertEq(fullMathHuff.mulDiv(1e18, 0, 1e18), 0);
-        assertEq(fullMathHuff.mulDiv(0, 0, 1e18), 0);
+        assertEq(fullMathLibrary.mulDiv(0, 1e18, 1e18), 0);
+        assertEq(fullMathLibrary.mulDiv(1e18, 0, 1e18), 0);
+        assertEq(fullMathLibrary.mulDiv(0, 0, 1e18), 0);
     }
 
     function testMulDivZeroDenominatorReverts() public {
         vm.expectRevert();
-        fullMathHuff.mulDiv(1e18, 1e18, 0);
+        fullMathLibrary.mulDiv(1e18, 1e18, 0);
     }
 
     function testMulDivUp() public {
-        assertEq(fullMathHuff.mulDivRoundingUp(2.5e27, 0.5e27, 1e27), 1.25e27);
-        assertEq(fullMathHuff.mulDivRoundingUp(2.5e18, 0.5e18, 1e18), 1.25e18);
-        assertEq(fullMathHuff.mulDivRoundingUp(2.5e8, 0.5e8, 1e8), 1.25e8);
-        assertEq(fullMathHuff.mulDivRoundingUp(369, 271, 1e2), 1000);
+        assertEq(
+            fullMathLibrary.mulDivRoundingUp(2.5e27, 0.5e27, 1e27),
+            1.25e27
+        );
+        assertEq(
+            fullMathLibrary.mulDivRoundingUp(2.5e18, 0.5e18, 1e18),
+            1.25e18
+        );
+        assertEq(fullMathLibrary.mulDivRoundingUp(2.5e8, 0.5e8, 1e8), 1.25e8);
+        assertEq(fullMathLibrary.mulDivRoundingUp(369, 271, 1e2), 1000);
 
-        assertEq(fullMathHuff.mulDivRoundingUp(1e27, 1e27, 2e27), 0.5e27);
-        assertEq(fullMathHuff.mulDivRoundingUp(1e18, 1e18, 2e18), 0.5e18);
-        assertEq(fullMathHuff.mulDivRoundingUp(1e8, 1e8, 2e8), 0.5e8);
+        assertEq(fullMathLibrary.mulDivRoundingUp(1e27, 1e27, 2e27), 0.5e27);
+        assertEq(fullMathLibrary.mulDivRoundingUp(1e18, 1e18, 2e18), 0.5e18);
+        assertEq(fullMathLibrary.mulDivRoundingUp(1e8, 1e8, 2e8), 0.5e8);
 
-        assertEq(fullMathHuff.mulDivRoundingUp(2e27, 3e27, 2e27), 3e27);
-        assertEq(fullMathHuff.mulDivRoundingUp(3e18, 2e18, 3e18), 2e18);
-        assertEq(fullMathHuff.mulDivRoundingUp(2e8, 3e8, 2e8), 3e8);
+        assertEq(fullMathLibrary.mulDivRoundingUp(2e27, 3e27, 2e27), 3e27);
+        assertEq(fullMathLibrary.mulDivRoundingUp(3e18, 2e18, 3e18), 2e18);
+        assertEq(fullMathLibrary.mulDivRoundingUp(2e8, 3e8, 2e8), 3e8);
     }
 
     function testMulDivUpEdgeCases() public {
-        assertEq(fullMathHuff.mulDivRoundingUp(0, 1e18, 1e18), 0);
-        assertEq(fullMathHuff.mulDivRoundingUp(1e18, 0, 1e18), 0);
-        assertEq(fullMathHuff.mulDivRoundingUp(0, 0, 1e18), 0);
+        assertEq(fullMathLibrary.mulDivRoundingUp(0, 1e18, 1e18), 0);
+        assertEq(fullMathLibrary.mulDivRoundingUp(1e18, 0, 1e18), 0);
+        assertEq(fullMathLibrary.mulDivRoundingUp(0, 0, 1e18), 0);
     }
 
     function testMulDivUpZeroDenominator() public {
         vm.expectRevert();
-        fullMathHuff.mulDivRoundingUp(1e18, 1e18, 0);
+        fullMathLibrary.mulDivRoundingUp(1e18, 1e18, 0);
     }
 
     function testFullMulDiv() public {
-        assertEq(fullMathHuff.mulDiv(0, 0, 1), 0);
-        assertEq(fullMathHuff.mulDiv(4, 4, 2), 8);
-        assertEq(fullMathHuff.mulDiv(2 ** 200, 2 ** 200, 2 ** 200), 2 ** 200);
+        assertEq(fullMathLibrary.mulDiv(0, 0, 1), 0);
+        assertEq(fullMathLibrary.mulDiv(4, 4, 2), 8);
+        assertEq(
+            fullMathLibrary.mulDiv(2 ** 200, 2 ** 200, 2 ** 200),
+            2 ** 200
+        );
     }
 
     function testFullMulDivUpRevertsIfRoundedUpResultOverflowsCase1() public {
         vm.expectRevert();
-        fullMathHuff.mulDivRoundingUp(
+        fullMathLibrary.mulDivRoundingUp(
             535006138814359,
             432862656469423142931042426214547535783388063929571229938474969,
             2
@@ -84,7 +103,7 @@ contract FullMathHuffTest is Test {
 
     function testFullMulDivUpRevertsIfRoundedUpResultOverflowsCase2() public {
         vm.expectRevert();
-        fullMathHuff.mulDivRoundingUp(
+        fullMathLibrary.mulDivRoundingUp(
             115792089237316195423570985008687907853269984659341747863450311749907997002549,
             115792089237316195423570985008687907853269984659341747863450311749907997002550,
             115792089237316195423570985008687907853269984653042931687443039491902864365164
@@ -98,7 +117,7 @@ contract FullMathHuffTest is Test {
     ) public returns (uint256 result) {
         if (d == 0) {
             vm.expectRevert();
-            fullMathHuff.mulDiv(a, b, d);
+            fullMathLibrary.mulDiv(a, b, d);
             return 0;
         }
 
@@ -120,11 +139,11 @@ contract FullMathHuffTest is Test {
         }
         if (prod1 >= d) {
             vm.expectRevert();
-            fullMathHuff.mulDiv(a, b, d);
+            fullMathLibrary.mulDiv(a, b, d);
             return 0;
         }
 
-        uint256 q = fullMathHuff.mulDiv(a, b, d);
+        uint256 q = fullMathLibrary.mulDiv(a, b, d);
         uint256 r = mulmod(a, b, d);
 
         // Compute q * d + r in Chinese Remainder Basis
@@ -147,12 +166,12 @@ contract FullMathHuffTest is Test {
             if (mulmod(a, b, d) > 0) {
                 if (!(mulDivResult < type(uint256).max)) {
                     vm.expectRevert();
-                    fullMathHuff.mulDivRoundingUp(a, b, d);
+                    fullMathLibrary.mulDivRoundingUp(a, b, d);
                     return;
                 }
                 expectedResult++;
             }
-            assertEq(fullMathHuff.mulDivRoundingUp(a, b, d), expectedResult);
+            assertEq(fullMathLibrary.mulDivRoundingUp(a, b, d), expectedResult);
         }
     }
 
@@ -162,12 +181,15 @@ contract FullMathHuffTest is Test {
             if (denominator == 0 || (x != 0 && (x * y) / x != y)) return;
         }
 
-        assertEq(fullMathHuff.mulDiv(x, y, denominator), (x * y) / denominator);
+        assertEq(
+            fullMathLibrary.mulDiv(x, y, denominator),
+            (x * y) / denominator
+        );
     }
 
     function testMulDivZeroDenominatorReverts(uint256 x, uint256 y) public {
         vm.expectRevert();
-        fullMathHuff.mulDiv(x, y, 0);
+        fullMathLibrary.mulDiv(x, y, 0);
     }
 
     function testMulDivUp(uint256 x, uint256 y, uint256 denominator) public {
@@ -177,24 +199,13 @@ contract FullMathHuffTest is Test {
         }
 
         assertEq(
-            fullMathHuff.mulDivRoundingUp(x, y, denominator),
+            fullMathLibrary.mulDivRoundingUp(x, y, denominator),
             x * y == 0 ? 0 : (x * y - 1) / denominator + 1
         );
     }
 
     function testMulDivUpZeroDenominatorReverts(uint256 x, uint256 y) public {
         vm.expectRevert();
-        fullMathHuff.mulDivRoundingUp(x, y, 0);
-    }
-
-    function testDifferential(uint256 x, uint256 y, uint256 d) external {
-        FullMath fullMathOfficial = new FullMath();
-
-        try fullMathHuff.mulDiv(x, y, d) returns (uint256 huffResult) {
-            assertEq(huffResult, fullMathOfficial.mulDiv(x, y, d));
-        } catch {
-            vm.expectRevert();
-            fullMathOfficial.mulDiv(x, y, d);
-        }
+        fullMathLibrary.mulDivRoundingUp(x, y, 0);
     }
 }
